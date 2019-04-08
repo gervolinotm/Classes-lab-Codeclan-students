eb68d0cb976b806526be8587f550ae36f2f2534b
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-08 17:18:49 +0100

    Sports Team - Lab Part B Completed

--- specs/sports_teams_specs.rb
+++ specs/sports_teams_specs.rb
@@ -42,5 +42,26 @@ class TestSportsTeam < MiniTest::Test
       sports_team.coach = "Billy Bob Bob"
       assert_equal("Billy Bob Bob", sports_team.coach)
     end
+
+    def test_add_player_to_team
+      players = ["David Hasselhof", "Zoe Ball", "Kermit T Frog"]
+      sports_team = SportsTeam.new("Manchester United", players, "Terry McTerry")
+      add_new_player = sports_team.add_players_to_team("Terrence Gol")
+      assert_equal(["David Hasselhof", "Zoe Ball", "Kermit T Frog", "Terrence Gol"], add_new_player)
+    end
+
+    def test_find_player_within_team_players
+      players = ["David Hasselhof", "Zoe Ball", "Kermit T Frog"]
+      sports_team = SportsTeam.new("Manchester United", players, "Terry McTerry")
+      player_search = sports_team.find_player("Zoe Ball")
+      assert_equal(true, player_search)
+    end
+
+    def test_number_of_wins
+      players = ["David Hasselhof", "Zoe Ball", "Kermit T Frog"]
+      sports_team = SportsTeam.new("Manchester United", players, "Terry McTerry")
+      number_of_wins = sports_team.total_number_of_wins("win")
+      assert_equal(1, number_of_wins)
+    end
 end
 # ["David Hasselhof", "Zoe Ball", "Kermit T Frog" ]
--- sports_teams.rb
+++ sports_teams.rb
@@ -6,6 +6,22 @@ class SportsTeam
       @team_name = team_name
       @players = players
       @coach = coach
+      @points = 0
     end
 
-end      
+    def add_players_to_team(new_player)
+      @players << new_player
+    end
+
+    def find_player(player_name)
+      for player in @players
+        return true if player == player_name
+      end
+    end
+
+    def total_number_of_wins(match_outcome)
+    @points += 1  if match_outcome == "win"
+    #  return @points  
+    end
+
+end
